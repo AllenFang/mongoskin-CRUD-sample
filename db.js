af7ac889ef795888e1f48835fcfffa36a3e6c54e
AllenFang <ayu780129@hotmail.com>
2015-12-18 23:52:18 +0800

    integrate with travis

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 var mongoskin = require("mongoskin");
-var db = mongoskin.db("mongodb://localhost:27017/allen", {native_parser: true});
+var db = mongoskin.db("mongodb://localhost:27017/mydb_test", {native_parser: true});
 
 module.exports.db = db;
